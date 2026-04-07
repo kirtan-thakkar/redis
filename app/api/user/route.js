f68737bcad0e87516e2c4752850f9e655a713234
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-04-07 22:25:09 +0530

    Add basic get and set commands -? redis

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,7 +1,13 @@
 import { NextResponse } from "next/server";
-
+import {redis} from "../../../lib/redis";
 export async function GET(request){
+    await redis.set("user:1", "kirtan");
+    await redis.set("user:2","thakkar")
+    await redis.set("user:3","All is well")
+
+    await redis.get("user:1");
+    const result = await redis.mget("user:1","user:2","user:3")
     return NextResponse.json({
-        message:"Hello world from nextjs and learning redis mother fucker",
-    },{status:201})
+        message: result
+    })
 }
