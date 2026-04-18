193bff3942bd57a33e9ac6563014a1fb07620b17
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-04-18 23:58:13 +0530

    fix

--- app/page.jsx
+++ app/page.jsx
@@ -4,9 +4,14 @@ export default function Home() {
   return (
     <div>
       <div className="container">
-        <div className="flex justify-center items-center h-screen bg-neutral-800">
+        <div className="flex  w-full mx-auto justify-center items-center h-[400px] bg-neutral-800">
           
-            <div className="py-4 px-4">
+            <div className="py-4 px-4 bg-neutral-300 rounded-3xl shadow-2xl flex items-center h-full w-full">
+              <h1>Hello world</h1>
+
+            </div>
+            <div className="py-4 px-4 bg-neutral-300 rounded-3xl shadow-2xl flex items-center h-full w-full">
+              <h1>How are you</h1>
 
             </div>
 
