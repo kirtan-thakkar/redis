85475def955935bcf5a72f6621feef48e078ae46
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-04-19 00:19:22 +0530

    Fix div not centering

--- app/page.jsx
+++ app/page.jsx
@@ -3,20 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="container">
-        <div className="flex  w-full mx-auto justify-center items-center h-[400px] bg-neutral-800">
+        <div className="flex w-280 mx-auto min-h-screen bg-neutral-800">
           
-            <div className="py-4 px-4 bg-neutral-300 rounded-3xl shadow-2xl flex items-center h-full w-full">
+            <div className="py-4 mx-auto bg-neutral-300 rounded-3xl shadow-2xl flex items-center h-full w-full">
               <h1>Hello world</h1>
 
             </div>
-            <div className="py-4 px-4 bg-neutral-300 rounded-3xl shadow-2xl flex items-center h-full w-full">
+            <div className="py-4 px-4 mx-auto bg-neutral-300 rounded-3xl shadow-2xl flex items-center h-full w-full">
               <h1>How are you</h1>
 
             </div>
 
         </div>
-      </div>
     </div>
   );
 }
