44cabedc2a2932683c4d08dc887e24d9d4d03ea7
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-04-18 23:55:34 +0530

    Initialse ui revamping

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Learning Redis</h1>
+      <div className="container">
+        <div className="flex justify-center items-center h-screen bg-neutral-800">
+          
+            <div className="py-4 px-4">
+
+            </div>
+
+        </div>
+      </div>
     </div>
   );
 }
