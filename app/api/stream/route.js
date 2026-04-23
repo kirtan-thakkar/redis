cd7e854f05550299ed83025372cf2705831c9e95
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-04-23 23:01:20 +0530

    Add another stream

--- app/api/stream/route.js
+++ app/api/stream/route.js
@@ -4,8 +4,15 @@ import { NextResponse } from "next/server";
 export async function GET(request){
     const stream = await redis.xadd("mystream", "*", { name: "kirtan" })
 
+    const res1 = await redis.xadd("1ststream","*",{
+        name:"kkirtan",
+        age:"23",
+        interest:"coding",
+    })
+
     return NextResponse.json({
         message: "Stream entry added successfully",
         id: stream,
+        response : res1
     });
 }
